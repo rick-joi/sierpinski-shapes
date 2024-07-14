9cc3718e88488afd8086e9cd502e376ceb5e9c0a
Rick Joi <rick.joi@workiversary.com>
2024-07-14 18:13:43 -0400

    Fix that seems to fix, or at least delay, the out-of-memory error

--- app/view/shared/sierpinski-shape/stage-0.tsx
+++ app/view/shared/sierpinski-shape/stage-0.tsx
@@ -12,7 +12,9 @@ export default function Stage0({ idPrefix, size, color }: Props) {
     <polygon
       points={`0,0 ${size},${size} 0,${size}`}
       id={getStageId(0, idPrefix)}
-      style={{ fill: color, opacity: "67%" }}
+      fill={color}
+      opacity={"67%"}
+      //      style={{ fill: color, opacity: "67%" }}
     />
   );
 }
